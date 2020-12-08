9cceab353ead388c5358d6686a823f94a8194058
ytalm <75474524+ytalm@users.noreply.github.com>
2020-12-09 07:54:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -46,11 +46,11 @@ sed -i '$a src-git Whitecolor https://github.com/Whitestorys/openwrt-extra-packa
 #theme_argon='https://github.com/sypopo/luci-theme-argon-mc.git'  # 主题地址
 #openClash_url='https://github.com/vernesong/OpenClash.git'       # OpenClash包地址
 #adguardhome_url='https://github.com/rufengsuixing/luci-app-adguardhome.git' # adguardhome 包地址
-lienol_url='https://github.com/Lienol/openwrt-package.git'       # Lienol 包地址
-vssr_url_rely='https://github.com/jerrykuku/lua-maxminddb.git'   # vssr lua-maxminddb依赖
-vssr_url='https://github.com/jerrykuku/luci-app-vssr.git'        # vssr地址
-vssr_plus_rely='https://github.com/Leo-Jo-My/my.git'             # vssr_plus 依赖
-vssr_plus='https://github.com/Leo-Jo-My/luci-app-vssr-plus.git'  # vssr_plus 地址
+#lienol_url='https://github.com/Lienol/openwrt-package.git'       # Lienol 包地址
+#vssr_url_rely='https://github.com/jerrykuku/lua-maxminddb.git'   # vssr lua-maxminddb依赖
+#vssr_url='https://github.com/jerrykuku/luci-app-vssr.git'        # vssr地址
+#vssr_plus_rely='https://github.com/Leo-Jo-My/my.git'             # vssr_plus 依赖
+#vssr_plus='https://github.com/Leo-Jo-My/luci-app-vssr-plus.git'  # vssr_plus 地址
 #filter_url='https://github.com/destan19/OpenAppFilter.git'       # AppFilter 地址
 
 
