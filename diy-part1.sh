ff6c063fb8c6963cfc72fb75924a5fdf7fc18008
ytalm <75474524+ytalm@users.noreply.github.com>
2020-12-22 22:28:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -39,8 +39,9 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
-git clone https://github.com/jerrykuku/lua-maxminddb.git    # vssr lua-maxminddb依赖
-git clone https://github.com/jerrykuku/luci-app-vssr.git        # vssr地址
+git clone https://github.com/jerrykuku/lua-maxminddb.git                      # vssr lua-maxminddb依赖
+git clone https://github.com/jerrykuku/luci-app-vssr.git                      # vssr地址
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner         # Add luci-app-onliner (need luci-app-nlbwmon)
 
 # rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 # 全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
